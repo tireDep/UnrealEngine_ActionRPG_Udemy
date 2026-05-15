b175ed2d9babfb4dca3654432a3446f749139762
DESKTOP-94M8TSN\wing9 <wing997@naver.com>
2026-05-16 00:15:12 +0900

    디버그 헬퍼 추가

--- Warrior/Source/Warrior/Public/Characters/WarriorHeroCharacter.h
+++ Warrior/Source/Warrior/Public/Characters/WarriorHeroCharacter.h
@@ -1,17 +1,16 @@
-// Fill Out this section
 
 #pragma once
 
 #include "CoreMinimal.h"
+#include "WarriorBaseCharacter.h"
 #include "Characters/WarriorBaseCharacter.h"
 #include "WarriorHeroCharacter.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class WARRIOR_API AWarriorHeroCharacter : public AWarriorBaseCharacter
 {
 	GENERATED_BODY()
-	
+
+protected:
+	virtual void BeginPlay() override;
 };
