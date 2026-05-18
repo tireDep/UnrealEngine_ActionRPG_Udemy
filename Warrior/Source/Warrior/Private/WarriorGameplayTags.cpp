4a19e29d83741a7bb0d5af2dc0fcb69f8a88442d
DESKTOP-94M8TSN\wing9 <wing997@naver.com>
2026-05-18 23:46:48 +0900

    네이티브 게임플레이 태그

---
+++ Warrior/Source/Warrior/Private/WarriorGameplayTags.cpp
@@ -0,0 +1,9 @@
+
+#include "WarriorGameplayTags.h"
+
+namespace  WarriorGameplayTags
+{
+	// >> Input Tags
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move, "InputTag.Move")
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look, "InputTag.Look")
+}
